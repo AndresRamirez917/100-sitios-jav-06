d54880f18a3b2f8f31ae355e6ddca49ec9dc7b1b
AndresRamirez917 <andresmintic2021@gmail.com>
2024-08-29 19:03:45 -0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,58 @@
+async function getData(){
+    const result = await fetch('https://rickandmortyapi.com/api/character');
+    const data = await result.json();
+    console.log(result)
+    data.results.forEach(element => {
+        if(element.id <= 3){
+            const card = document.createRange().createContextualFragment(`
+                
+                <div class="card">
+                    <img src="${element.image}" alt="">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit consectetur repellat magni praesentium delectus maiores! Aperiam labore iste fugiat ipsum, nostrum natus quae veniam harum velit. Error officiis, vitae perferendis aliquam, ipsa soluta minus adipisci ex distinctio inventore accusamus quasi?</p>
+                 </div>
+                
+                `)
+                const services_row = document.querySelector('.services-row')
+                services_row.append(card)
+        }
+    });
+}
+
+const btn_submit = document.getElementById('btn-submit');
+
+const validar = (e) => {
+    e.preventDefault();
+    const nombre = document.getElementById('nombre');
+    const email = document.getElementById('email');
+    const mensaje = document.getElementById('message');
+    const email_valido = email => {
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+    }
+    if(nombre.value == ""){
+        alert("El nombre no puede esta vacío")
+        return false;
+    }
+    
+    if(email.value == ""){
+        alert("El email no puede estar vacío")
+        return false
+    }
+
+    if(mensaje.value == ""){
+        alert("El mensaje no puede estar vacío")
+        return false;
+    }
+    if(!email_valido(email.value)){
+        alert("El formato del email no es valido")  
+        return false      
+    }
+    nombre.value="";
+    email.value="";
+    mensaje.value="";
+    return true
+
+}
+btn_submit.addEventListener("click", validar)
+
+getData()
+
